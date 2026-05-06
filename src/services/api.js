ed0729b254a239c0541c8b166f9794af26c7e569
Leonan Thomaz <leonan.thomaz@gmail.com>
2026-05-06 13:41:04 -0300

    Adicionando url de servidor local

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 
-const API_BASE_URL = 'https://vr-beneficios-backend-fedcorp-ju482.ondigitalocean.app/api';
+const API_BASE_URL = "http://localhost:8000/api";
+// const API_BASE_URL = 'https://vr-beneficios-backend-fedcorp-ju482.ondigitalocean.app/api';
 const ACCESS_TOKEN_KEY = 'accessToken';
 
 /**
