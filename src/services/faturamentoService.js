0d69e779dc079ba3dd9037cb7ea09b1f0ca149ed
Leonan Thomaz <leonan.thomaz@gmail.com>
2026-05-14 14:26:28 -0300

    Alterando url em faturamento - parte 2

--- src/services/faturamentoService.js
+++ src/services/faturamentoService.js
@@ -185,8 +185,8 @@ export const faturamentoService = {
 
     const queryString = query.toString()
 
-    const API_BASE_URL = 'https://vr-beneficios-backend-fedcorp-ju482.ondigitalocean.app/api';
-    // const API_BASE_URL = "http://localhost:8000/api";
+    const API_BASE_URL = 'https://vr-beneficios-backend-fedcorp-ju482.ondigitalocean.app';
+    // const API_BASE_URL = "http://localhost:8000";
 
 
     const API_BASE = API_BASE_URL
