fddddb81705adac92ab1b98b32d2cf578817475a
Leonan Thomaz <leonan.thomaz@gmail.com>
2026-05-14 14:25:18 -0300

    Alterando url em faturamento

--- src/services/faturamentoService.js
+++ src/services/faturamentoService.js
@@ -185,8 +185,11 @@ export const faturamentoService = {
 
     const queryString = query.toString()
 
-    const API_BASE =
-      import.meta.env.VITE_API_URL || 'http://localhost:8000'
+    const API_BASE_URL = 'https://vr-beneficios-backend-fedcorp-ju482.ondigitalocean.app/api';
+    // const API_BASE_URL = "http://localhost:8000/api";
+
+
+    const API_BASE = API_BASE_URL
 
     return `${API_BASE}/api/upload/export/faturamento/${
       queryString ? `?${queryString}` : ''
